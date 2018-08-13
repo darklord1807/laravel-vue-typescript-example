e32c7ef33137f7be736ae5fae5679b5a566f4e33
121there <121there@gmail.com>
2018-08-13 19:09:38 +0900

    Remove semicolon

--- resources/assets/ts/bootstrap.ts
+++ resources/assets/ts/bootstrap.ts
@@ -1,26 +1,25 @@
-import Axios, { AxiosStatic } from 'axios';
+import Axios, {AxiosStatic} from 'axios'
 
 declare global {
     interface Window {
-        axios: AxiosStatic;
+        axios: AxiosStatic
     }
+
     interface Element {
-        content: string;
+        content: string
     }
 }
 
 export default function bootstrap() {
+    window.axios = Axios
+    window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-    window.axios = Axios;
-
-    window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-    let token = document.head.querySelector('meta[name="csrf-token"]');
+    let token = document.head.querySelector('meta[name="csrf-token"]')
 
     if (token) {
-        window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+        window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
     } else {
-        console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+        console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token')
     }
 
 }
--- resources/assets/ts/components/ExampleComponent.vue
+++ resources/assets/ts/components/ExampleComponent.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script lang="ts">
-    import {Vue, Component} from 'vue-property-decorator';
+    import {Vue, Component} from 'vue-property-decorator'
 
     @Component
     export default class ExampleComponent extends Vue {
